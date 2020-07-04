398332b0c7777862e9988c833633df0b874e845a
GaryHjy <782316959@qq.com>
2020-07-04 15:40:09 +0800

    feat: init

---
+++ src/views/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    count:
+    {{ count }}
+    doubleCount:
+    {{ doubleCount }}
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed, watch } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    const add = () => {
+      count.value++;
+    };
+    const doubleCount = computed(() => count.value * 2);
+    watch(
+      () => count.value,
+      val => {
+        console.log("watch", val);
+      }
+    );
+    return {
+      count,
+      doubleCount,
+      add
+    };
+  }
+};
+</script>

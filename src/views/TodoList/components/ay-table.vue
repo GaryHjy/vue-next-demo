7a7c0442e00cf03bd20430ec80c23d032e10e286
GaryHjy <782316959@qq.com>
2020-07-06 16:55:45 +0800

    feat: 添加ay-table

---
+++ src/views/TodoList/components/ay-table.vue
@@ -0,0 +1,133 @@
+<script>
+import { h } from "vue";
+
+export default {
+  name: "AyTable",
+  props: {
+    columns: {
+      type: Array,
+      default: () => []
+    },
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup(props) {
+    const renderContent = val =>
+      h(
+        "div",
+        {
+          class: "cell"
+        },
+        val
+      );
+
+    const renderHeader = () =>
+      h(
+        "thead",
+        {
+          class: "ay-table__header"
+        },
+        h(
+          "tr",
+          props.columns.map(column => h("th", renderContent(column.label)))
+        )
+      );
+
+    const renderBody = () =>
+      h(
+        "tbody",
+        {
+          class: "ay-table__body"
+        },
+        props.data.map(item =>
+          h(
+            "tr",
+            {
+              class: "ay-table__row"
+            },
+            props.columns.map(column =>
+              h("td", renderContent(item[column.prop]))
+            )
+          )
+        )
+      );
+
+    return () =>
+      h(
+        "table",
+        {
+          class: "ay-table"
+        },
+        [renderHeader(), renderBody()]
+      );
+  }
+};
+</script>
+
+// TODO scoped有bug
+<style lang="scss">
+.ay-table {
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 14px;
+  color: #606266;
+
+  .cell {
+    box-sizing: border-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    word-break: break-all;
+    line-height: 23px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+
+  &__header {
+    color: #909399;
+    font-weight: 500;
+    th {
+      padding: 12px 0;
+      min-width: 0;
+      box-sizing: border-box;
+      text-overflow: ellipsis;
+      vertical-align: middle;
+      position: relative;
+      text-align: left;
+      border-bottom: 1px solid #ebeef5;
+    }
+    .cell {
+      display: inline-block;
+      box-sizing: border-box;
+      position: relative;
+      vertical-align: middle;
+      padding-left: 10px;
+      padding-right: 10px;
+      width: 100%;
+    }
+  }
+
+  &__body {
+    tr {
+      &:hover {
+        td {
+          background-color: #f5f7fa;
+        }
+      }
+    }
+    td {
+      padding: 12px 0;
+      min-width: 0;
+      box-sizing: border-box;
+      text-overflow: ellipsis;
+      vertical-align: middle;
+      position: relative;
+      text-align: left;
+      border-bottom: 1px solid #ebeef5;
+      transition: background-color 0.25s ease;
+    }
+  }
+}
+</style>

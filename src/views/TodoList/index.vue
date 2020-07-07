52700dc32ba683f4ff25128e73f54fc88f1df4fa
GaryHjy <782316959@qq.com>
2020-07-07 11:27:26 +0800

    feat: 添加todolist页面

--- src/views/TodoList/index.vue
+++ src/views/TodoList/index.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
-    {{ text }}
+  <div class="todo-list">
+    <button>添加</button>
+    <ay-table
+      :columns="columns"
+      :data="data"
+      :operationButtons="operationButtons"
+    />
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import AyTable from "./components/ay-table";
+
 export default {
-  data() {
+  components: {
+    AyTable
+  },
+  setup() {
+    const columns = ref([
+      {
+        prop: "content",
+        label: "内容"
+      }
+    ]);
+    const data = ref([
+      {
+        content: "hhhhhhhhhhhh"
+      }
+    ]);
+    const operationButtons = ref([
+      {
+        text: "完成",
+        onClick: row => {
+          console.log(row.content);
+        }
+      },
+      {
+        text: "删除",
+        style: {
+          color: "red"
+        },
+        onClick: row => {
+          console.log(row.content);
+        }
+      }
+    ]);
+
     return {
-      text: "todoList"
+      columns,
+      data,
+      operationButtons
     };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.todo-list {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
